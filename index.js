8cff1be915e6436a995f4365bfacdfa4865e2289
Vladimir Semenov <thesun2003@gmail.com>
2017-10-29 20:29:42 +1300

    Added a Redirect option

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ let index_html = 'URL is required';
 app.get('/', function(req, res) {
 	// let url = 'https://t.co/eR2JEWlrAn';
 	let url = req.query.url;
+	let redirect = !!req.query.redirect;
 
 	if (url) {
 		request(url, function (error, response, body) {
 		  	// console.log('statusCode:', response && response.statusCode);
 		  	// console.log('URL:', response && response.request.uri.href);
-			res.status(200).send(response.request.uri.href);
+
+		  	let result_url = response.request.uri.href;
+
+		  	if (redirect) {
+				res.redirect(301, result_url);
+		  	} else {
+				res.status(200).send(result_url);
+			}
 		});
 
 		console.log('Requested: ', url);
